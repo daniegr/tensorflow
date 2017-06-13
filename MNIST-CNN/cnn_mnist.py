fac17440b369db0d5ec19bbdd463a954f8fd592f
Daniel Groos <daniegr@stud.ntnu.no>
2017-06-13 09:33:17 +0200

    Set up training process, logging during training process and evaluation process

--- MNIST-CNN/cnn_mnist.py
+++ MNIST-CNN/cnn_mnist.py
@@ -116,14 +116,14 @@ def cnn_model_fn(features, labels, mode):
           train_op=train_op)
   
   return model
-ta = mnist.train.images # Returns np.array
-  train_l
+
 # Train and evaluate classifier
 def main(unused_argv):
     
   # Load training and eval data
   mnist = learn.datasets.load_dataset("mnist")
-  train_daabels = np.asarray(mnist.train.labels, dtype=np.int32)
+  train_data = mnist.train.images # Returns np.array
+  train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
   eval_data = mnist.test.images # Returns np.array
   eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
   
@@ -132,6 +132,35 @@ def main(unused_argv):
           model_fn=cnn_model_fn, 
           model_dir="/tmp/mnist_convnet_model")
   
+  # Set up logging for predictions
+  tensors_to_log = {"probabilities": "softmax_tensor"}
+  logging_hook = tf.train.LoggingTensorHook(
+          tensors=tensors_to_log, 
+          every_n_iter=50)
+  
+  # Train the model
+  mnist_classifier.fit(
+          x=train_data,
+          y=train_labels,
+          batch_size=100,
+          steps=20000,
+          monitors=[logging_hook])
+  
+  # Configure the accuracy metric for evaluation
+  metrics = {
+          "accuracy":
+                learn.MetricSpec(
+                        metric_fn=tf.metrics.accuracy, 
+                        prediction_key="classes"),
+  }
+  
+  # Evaluate the model and print results
+  eval_results = mnist_classifier.evaluate(
+          x=eval_data, 
+          y=eval_labels, 
+          metrics=metrics)
+  print(eval_results)    
+      
 if __name__ == "__main__":
   tf.app.run()
 
